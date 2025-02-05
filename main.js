8903da16c9a6a0ca22ff08fc8f3f0a3b1f924488
AndryGamerTV <asdaartefret@gmail.com>
2025-02-05 11:25:52 +0200

    update all errors, eslint errors update

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
-import $ from 'jquery';
-import paper from 'paper';
+$(document).ready(function() {
+    'use strict';
+    paper.install(window);
+    paper.setup(document.getElementById('mainCanvas'));
 
-$(document).ready(() => {
-      'use strict';
-      paper.install(window);
-      paper.setup(document.getElementById('mainCanvas'));
+    var tool = new Tool();
+    var c = Shape.Circle(200, 200, 80);
+    c.fillColor = 'black';
+    var text = new PointText(200, 200);
+    text.justification = 'center';
+    text.fillColor = 'white';
+    text.fontSize = 20;
+    text.content = 'Hello World!';
 
-      const tool = new paper.Tool();
-      const c = new paper.Path.Circle(new paper.Point(200, 200), 80);
-      c.fillColor = 'black';
-      const text = new paper.PointText(new paper.Point(200, 200));
-      text.justification = 'center';
-      text.fillColor = 'white';
-      text.fontSize = 20;
-      text.content = 'Hello World!';
-
-      tool.onMouseDown = (event) => {
-            const c = new paper.Path.Circle(event.point, 10);
-            c.fillColor = 'green';
-      };
-
-      paper.view.draw();
+    tool.onMouseDown = function(event) {
+        var c = Shape.Circle(event.point.x, event.point.y, 20);
+        c.fillColor = 'green';
+    };
+    paper.view.draw();
 });
